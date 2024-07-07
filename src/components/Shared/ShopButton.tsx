ea359fc6a44d1e3251c2e49974350afeade61e59
Gabriel Zalewski <gabrielz2702@gmail.com>
2024-07-07 16:56:15 +0200

    Add reusable button component

---
+++ src/components/Shared/ShopButton.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ShopButton: React.FC = () => {
+  return (
+    <button className='pb-1 mt-2 border-b-4 border-red-500 font-bold text-sm'>
+        SPRAWDŹ OFERTĘ
+    </button>
+  )
+};
+
+export default ShopButton;
