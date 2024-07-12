4560a939570f3c8a426d2e9822bbec567abf3dc1
Gabriel Zalewski <gabrielz2702@gmail.com>
2024-07-12 19:27:24 +0200

    Create file with PromotedProducts data and fix some styling

--- src/components/PromotedProducts/PromotedProducts.tsx
+++ src/components/PromotedProducts/PromotedProducts.tsx
@@ -3,65 +3,7 @@ import Slider from 'react-slick';
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
 import { NextArrow, PrevArrow } from '../Shared/CustomArrows';
-import Product1 from '../../assets/images/products/Product1.jpg';
-import Product2 from '../../assets/images/products/Product2.jpg';
-import Product3 from '../../assets/images/products/Product3.jpg';
-import Product4 from '../../assets/images/products/Product4.jpg';
-import Product5 from '../../assets/images/products/Product5.jpg';
-import Product6 from '../../assets/images/products/Product6.jpg';
-import Product7 from '../../assets/images/products/Product7.jpg';
-
-interface Product {
-    id: number,
-    image: string;
-    title: string;
-    price: string;
-};
-
-const products: Product[] = [
-    {
-        id: 1,
-        image: Product1,
-        title: 'Czerwone spodnie w paski',
-        price: '115 PLN',
-    },
-    {
-        id: 2,
-        image: Product2,
-        title: 'Białe ogrodniczki',
-        price: '100 PLN',
-    },
-    {
-        id: 3,
-        image: Product3,
-        title: 'Koszula w kratę',
-        price: '70 PLN',
-    },
-    {
-        id: 4,
-        image: Product4,
-        title: 'Biały t-shirt',
-        price: '55 PLN',
-    },
-    {
-        id: 5,
-        image: Product5,
-        title: 'Czarna bluzka',
-        price: '130 PLN',
-    },
-    {
-        id: 6,
-        image: Product6,
-        title: 'Niebieskie Jeansy',
-        price: '90 PLN',
-    },
-    {
-        id: 7,
-        image: Product7,
-        title: 'Biała koszulka',
-        price: '45 PLN',
-    },
-];
+import products from '../../data/products';
 
 const PromotedProducts: React.FC = () => {
     const settings = {
@@ -101,20 +43,20 @@ const PromotedProducts: React.FC = () => {
     };
 
     return (
-        <div className='mx-auto px-8 font-domine'>
-            <div className='p-6 flex justify-between'>
-                <h2 className='text-2xl font-bold'>
+        <div className='mx-8'>
+            <div className='mt-4 mx-10 flex justify-between'>
+                <h2 className='font-cookie text-4xl font-bold'>
                     Dzisiejsza Selekcja
                 </h2>
             </div>
             <Slider {...settings}>
                 {products.map(product => (
-                    <div key={product.id} className='px-2'>
-                        <div className='rounded-lg p-4 shadow'>
+                    <div key={product.id} className='px-2 py-5'>
+                        <div className='rounded-lg p-4 shadow transition-transform transform hover:scale-105 duration-500 cursor-pointer font-domine'>
                             <img
                                 src={product.image}
                                 alt={product.title}
-                                className='h-60 w-full object-cover mb-4 rounded'
+                                className='h-96 w-full object-cover mb-4 rounded'
                             />
                             <h3 className='text-lg'>
                                 {product.title}
---
+++ src/data/products.ts
@@ -0,0 +1,61 @@
+import Product1 from '../assets/images/products/Product1.jpg'
+import Product2 from '../assets/images/products/Product2.jpg'
+import Product3 from '../assets/images/products/Product3.jpg'
+import Product4 from '../assets/images/products/Product4.jpg'
+import Product5 from '../assets/images/products/Product5.jpg'
+import Product6 from '../assets/images/products/Product6.jpg'
+import Product7 from '../assets/images/products/Product7.jpg'
+
+interface Product {
+    id: number,
+    image: string;
+    title: string;
+    price: string;
+};
+
+const products: Product[] = [
+    {
+        id: 1,
+        image: Product1,
+        title: 'Czerwone spodnie w paski',
+        price: '115 PLN',
+    },
+    {
+        id: 2,
+        image: Product2,
+        title: 'Białe ogrodniczki',
+        price: '100 PLN',
+    },
+    {
+        id: 3,
+        image: Product3,
+        title: 'Koszula w kratę',
+        price: '70 PLN',
+    },
+    {
+        id: 4,
+        image: Product4,
+        title: 'Biały t-shirt',
+        price: '55 PLN',
+    },
+    {
+        id: 5,
+        image: Product5,
+        title: 'Czarna bluzka',
+        price: '130 PLN',
+    },
+    {
+        id: 6,
+        image: Product6,
+        title: 'Niebieskie Jeansy',
+        price: '90 PLN',
+    },
+    {
+        id: 7,
+        image: Product7,
+        title: 'Biała koszulka',
+        price: '45 PLN',
+    },
+];
+
+export default products;
