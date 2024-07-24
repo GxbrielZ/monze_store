ffbd9558c46fa3ca4d676b921daedd2dca6ec585
Gabriel Zalewski <gabrielz2702@gmail.com>
2024-07-24 14:37:00 +0200

    Update navbar links

--- src/data/menuData.ts
+++ src/data/menuData.ts
@@ -1,33 +1,14 @@
 interface MenuItem {
-    [key: string]: string[];
+    name: string;
+    link: string;
 }
 
-interface MenuItems {
-    [key: string]: MenuItem;
-}
+const menuData: MenuItem[] = [
+    { name: 'Kobieta', link: '/woman' },
+    { name: 'Mężczyzna', link: '/man' },
+    { name: 'Dziecko', link: '/child' },
+    { name: 'Akcesoria', link: '/accesories' },
+    { name: 'Dekoracje', link: '/decorations' }
+];
 
-export const menuItems: MenuItems = {
-    KOBIETA: {
-        ODZIEŻ: ['Bluzki', 'Bluzy', 'Kamizelki', 'Koszule', 'Koszulki', 'Kurtki/Płaszcze', 'Marynarki', 'Polo', 'Spódnice', 'Sukienki', 'Swetry'],
-        SPODNIE: ['Jeansy', 'Spodnie dresowe', 'Spodnie sztruksowe', 'Szorty'],
-        OBUWIE: ['Botki', 'Kapcie', 'Klapki', 'Półbuty', 'Trampki', 'Sandały', 'Sneakersy', 'Śniegowce', 'Trapery', 'Trekkingi'],
-        DODATKI: ['Akcesoria', 'Bielizna', 'Czapki/Kapelusze', 'Kosmetyki', 'Okulary', 'Paski', 'Portfele', 'Rękawiczki', 'Skarpetki', 'Szaliki/Apaszki', 'Torby/Plecaki']
-    },
-    MĘŻCZYZNA: {
-        ODZIEŻ: ['Bluzki', 'Bluzy', 'Kamizelki', 'Koszule', 'Koszulki', 'Kurtki/Płaszcze', 'Marynarki', 'Polo', 'Spódnice', 'Sukienki', 'Swetry'],
-        SPODNIE: ['Jeansy', 'Spodnie dresowe', 'Spodnie sztruksowe', 'Szorty'],
-        OBUWIE: ['Botki', 'Kapcie', 'Klapki', 'Półbuty', 'Trampki', 'Sandały', 'Sneakersy', 'Śniegowce', 'Trapery', 'Trekkingi'],
-        DODATKI: ['Akcesoria', 'Bielizna', 'Czapki/Kapelusze', 'Kosmetyki', 'Okulary', 'Paski', 'Portfele', 'Rękawiczki', 'Skarpetki', 'Szaliki/Apaszki', 'Torby/Plecaki']
-    },
-    DZIECKO: {
-        ODZIEŻ: ['Bluzki', 'Bluzy', 'Kamizelki', 'Koszule', 'Koszulki', 'Kurtki/Płaszcze', 'Marynarki', 'Polo', 'Spódnice', 'Sukienki', 'Swetry'],
-        SPODNIE: ['Jeansy', 'Spodnie dresowe', 'Spodnie sztruksowe', 'Szorty'],
-        OBUWIE: ['Botki', 'Kapcie', 'Klapki', 'Półbuty', 'Trampki', 'Sandały', 'Sneakersy', 'Śniegowce', 'Trapery', 'Trekkingi'],
-        DODATKI: ['Akcesoria', 'Bielizna', 'Czapki/Kapelusze', 'Kosmetyki', 'Okulary', 'Paski', 'Portfele', 'Rękawiczki', 'Skarpetki', 'Szaliki/Apaszki', 'Torby/Plecaki']
-    },
-    AKCESORIA: {
-        AKCESORIA: ['Plecaki', 'Torby', 'Kolczyki', 'Naszyjniki']
-    }
-};
-
-export const mainMenuItems: string[] = ['KOBIETA', 'MĘŻCZYZNA', 'DZIECKO', 'AKCESORIA', 'DEKORACJE'];
+export default menuData;
