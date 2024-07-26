a76186a21f3813e22b86734ae93a4d299286cee9
Gabriel Zalewski <gabrielz2702@gmail.com>
2024-07-26 13:16:05 +0200

    Add responsive TitleBanner as a title for promoted products and make it position relative

--- src/components/PromotedProducts/PromotedProducts.tsx
+++ src/components/PromotedProducts/PromotedProducts.tsx
@@ -3,6 +3,7 @@ import Slider from 'react-slick';
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
 import { NextArrow, PrevArrow } from '../Shared/CustomArrows';
+import TitleBanner from '../Shared/TitleBanner';
 import products from '../../data/products';
 
 const PromotedProducts: React.FC = () => {
@@ -43,31 +44,29 @@ const PromotedProducts: React.FC = () => {
     };
 
     return (
-        <div className='mx-8'>
-            <div className='mt-4 mx-10 flex justify-between'>
-                <h2 className='font-cookie text-4xl font-bold'>
-                    Dzisiejsza Selekcja
-                </h2>
+        <div className='relative'>
+            <TitleBanner />
+            <div className='mx-8 relative z-2 -mt-28'>
+              <Slider {...settings}>
+                  {products.map(product => (
+                      <div key={product.id} className='px-2 py-5'>
+                          <div className='rounded-lg p-4 shadow transition-transform transform hover:scale-105 duration-500 cursor-pointer font-domine'>
+                              <img
+                                  src={product.image}
+                                  alt={product.title}
+                                  className='h-96 w-full object-cover mb-4 rounded'
+                              />
+                              <h3 className='text-lg'>
+                                  {product.title}
+                              </h3>
+                              <p className='text-red-500'>
+                                  {product.price}
+                              </p>
+                          </div>
+                      </div>
+                  ))}
+              </Slider>
             </div>
-            <Slider {...settings}>
-                {products.map(product => (
-                    <div key={product.id} className='px-2 py-5'>
-                        <div className='rounded-lg p-4 shadow transition-transform transform hover:scale-105 duration-500 cursor-pointer font-domine'>
-                            <img
-                                src={product.image}
-                                alt={product.title}
-                                className='h-96 w-full object-cover mb-4 rounded'
-                            />
-                            <h3 className='text-lg'>
-                                {product.title}
-                            </h3>
-                            <p className='text-red-500'>
-                                {product.price}
-                            </p>
-                        </div>
-                    </div>
-                ))}
-            </Slider>
         </div>
     )
 };
