7a25ac2eb21b734d85406d1543b5f02a835ba1b9
Gabriel Zalewski <gabrielz2702@gmail.com>
2024-07-31 14:10:25 +0200

    Add new components and change order

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,19 +1,23 @@
 import React from 'react';
 import Hero from '../components/Hero/Hero';
-import PromotedProducts from '../components/PromotedProducts/PromotedProducts';
+import ProductListing from '../components/ProductListing/ProductListing';
 import Commercial from '../components/Commercial/Commercial';
+import PromotedProducts from '../components/PromotedProducts/PromotedProducts';
 import PromotedAccesories from '../components/PromotedAccesories/PromotedAccesories';
 import InfoBar from '../components/InfoBar/InfoBar';
+import FanpageSection from '../components/FanpageSection/FanpageSection';
 import Footer from '../components/Footer/Footer';
 
 const Home: React.FC = () => {
   return (
     <>
       <Hero />
-      <PromotedProducts />
+      <ProductListing />
       <Commercial />
+      <PromotedProducts />
       <PromotedAccesories />
       <InfoBar />
+      <FanpageSection />
       <Footer />
     </>
   )
