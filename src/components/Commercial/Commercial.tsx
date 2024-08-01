f95787cc43032f146be47017d58d0e2289d39ef5
Gabriel Zalewski <gabrielz2702@gmail.com>
2024-08-01 15:38:41 +0200

    Create new commercial banner with changing backgound images

--- src/components/Commercial/Commercial.tsx
+++ src/components/Commercial/Commercial.tsx
@@ -1,38 +1,58 @@
 import React from 'react';
-import { motion } from 'framer-motion';
-import CommercialPic from '../../assets/images/CommercialPic.png';
+import { useState, useEffect } from 'react';
+import { motion, AnimatePresence } from 'framer-motion';
+import CommercialOne from '../../assets/images/Commercial/CommercialOne.jpg';
+import CommercialTwo from '../../assets/images/Commercial/CommercialTwo.jpg';
+import CommercialThree from '../../assets/images/Commercial/CommercialThree.jpg';
+
+const images = [CommercialOne, CommercialTwo, CommercialThree];
+const titles = [
+    'ODKRYJ SWÓJ STYL Z MONZE',
+    'ŚWIETNA JAKOŚĆ W ROZSĄDNEJ CENIE',
+    'ZANURZ SIĘ W ŚWIECIE ZAKUPÓW'
+];
 
 const Commercial: React.FC = () => {
-    const variants = {
-        hidden: { opacity: 0, x: 100 },
-        visible: { opacity: 1, x: 0 }
-    };
+    const [currentImage, setCurrentImage] = useState(0);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setCurrentImage((prevImage) => (prevImage + 1) % images.length);
+        }, 10000);
+
+        return () => clearInterval(interval);
+    }, []);
 
     return (
-        <div className='flex flex-col md:flex-row items-center px-4 lg:px-12 my-12 md:my-24 bg-gray-100 text-gray-900'>
-            <div className='w-full md:w-1/2 flex justify-center pt-4 md:pt-0'>
-                <img
-                    src={CommercialPic}
-                    alt="Fashion Model"
-                    className='rounded-lg shadow-lg bg-gradient-to-tr from-orange-200 to-pink-400'
+        <div className='relative w-full h-96 md:h-[40rem] mt-24'>
+            <AnimatePresence>
+                <motion.img
+                    key={currentImage}
+                    src={images[currentImage]}
+                    alt="Summer Sale"
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    exit={{ opacity: 0 }}
+                    className='object-cover w-full h-full absolute inset-0'
+                    transition={{ duration: 1 }}
                 />
+            </AnimatePresence>
+            <div className='absolute inset-0 flex items-center justify-center bg-black bg-opacity-30'>
+            <AnimatePresence mode="wait">
+                    <motion.div
+                        key={currentImage}
+                        initial={{ opacity: 0 }}
+                        animate={{ opacity: 1 }}
+                        exit={{ opacity: 0, transition: { duration: 0.3 } }}
+                        transition={{ duration: 1 }}
+                        className='absolute'
+                    >
+                        <h1 className='text-white text-center mx-2 sm:mx-0 text-4xl md:text-6xl font-bold'>
+                            {titles[currentImage]}
+                        </h1>
+                    </motion.div>
+                </AnimatePresence>
             </div>
-            <motion.div
-                className='w-full md:w-1/2 mt-6 md:mt-0 md:ml-6 text-center md:text-left'
-                initial='hidden'
-                whileInView='visible'
-                viewport={{ once: true }}
-                transition={{ duration: 1.5 }}
-                variants={variants}
-            >
-                <h1 className='font-cookie text-4xl xl:text-5xl 2xl:text-6xl font-bold mb-4 md:my-2 xl:my-4 2xl:my-6'>
-                    MonZe - Odkryj Swój Styl
-                </h1>
-                <p className='font-domine md:text-sm lg:text-md xl:text-lg w-11/12 md:w-[95%] lg:w-full mx-auto md:mx-0 mb-4'>
-                    Sprawdź kolekcje, które zachwycą Cię swoją różnorodnością i wyrafinowaniem. Od klasycznych, eleganckich strojów po nowoczesne, miejskie stylizacje - każdy znajdzie coś dla siebie. Nasze ubrania są wyrazem najnowszych trendów, a jednocześnie ponadczasowej elegancji.<br />
-                    Dołącz do grona zadowolonych klientów MonZe i odkryj świat, gdzie moda spotyka się z luksusem. Dzięki nam zawsze będziesz wyglądać stylowo i czuć się wyjątkowo.
-                </p>
-            </motion.div>
         </div>
     )
 };
