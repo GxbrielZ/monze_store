b8b95729822f4972184feedae41151a59cbe8bdd
Gabriel Zalewski <gabrielz2702@gmail.com>
2024-08-04 17:31:37 +0200

    Add functionality of adding and removing products to/from cart. When item is added to cart the add icon changes.

--- src/components/Cart/Cart.tsx
+++ src/components/Cart/Cart.tsx
@@ -1,37 +1,67 @@
 import React from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
 import { RxCross2 } from "react-icons/rx";
+import { useCart } from '../Cart/CartContext';
+import { FaTrash } from "react-icons/fa";
 
 interface CartProps {
     isOpen: boolean;
     toggleCart: () => void;
 }
 
-const Cart: React.FC<CartProps> = ({isOpen, toggleCart}) => {
-  return (
-    <AnimatePresence>
-        {isOpen && (
-            <motion.div
-                initial={{ opacity: 0, x: '100%' }}
-                animate={{ opacity: 1, x: 0 }}
-                exit={{ opacity: 0, x: '100%' }}
-                className='fixed top-0 right-0 w-full h-full lg:w-1/3 bg-white shadow-lg z-50'
-            >
-                <div className='p-4 flex justify-between items-center border-b'>
-                    <h2 className='text-xl font-semibold text-black'>
-                        Koszyk
-                    </h2>
-                    <button onClick={toggleCart} className='text-2xl text-black'>
-                        <RxCross2 />
-                    </button>
-                </div>
-                <div className='p-4'>
-                    <p>Your cart is empty.</p>
-                </div>
-            </motion.div>
-        )}
-    </AnimatePresence>
-  );
+const Cart: React.FC<CartProps> = ({ isOpen, toggleCart }) => {
+    const { cart, removeFromCart } = useCart();
+
+    return (
+        <AnimatePresence>
+            {isOpen && (
+                <motion.div
+                    initial={{ opacity: 0, x: '100%' }}
+                    animate={{ opacity: 1, x: 0 }}
+                    exit={{ opacity: 0, x: '100%' }}
+                    className='fixed top-0 right-0 w-full h-full lg:w-1/3 bg-white shadow-lg z-50'
+                >
+                    <div className='p-4 flex justify-between items-center border-b'>
+                        <h2 className='text-xl font-semibold text-black'>
+                            Koszyk
+                        </h2>
+                        <button onClick={toggleCart} className='text-2xl text-black'>
+                            <RxCross2 />
+                        </button>
+                    </div>
+                    <div className='px-4'>
+                        {cart.length === 0 ? (
+                            <p className='text-black'>Your cart is empty.</p>
+                        ) : (
+                            <ul>
+                                {cart.map(item => (
+                                    <li key={item.id} className='flex justify-between items-center border-b py-2'>
+                                        <div className='flex text-black gap-x-2'>
+                                            <img src={item.image} alt={item.title} className='w-16 h-16 object-cover' />
+                                            <div>
+                                                <p className='font-sans font-semibold'>
+                                                    {item.title}
+                                                </p>
+                                                <p className='text-red-600 font-sans font-semibold'>
+                                                    {item.price.toFixed(1)} PLN
+                                                </p>
+                                            </div>
+                                        </div>
+                                        <button
+                                            onClick={() => removeFromCart(item.id)}
+                                            className='text-black'
+                                        >
+                                            <FaTrash />
+                                        </button>
+                                    </li>
+                                ))}
+                            </ul>
+                        )}
+                    </div>
+                </motion.div>
+            )}
+        </AnimatePresence>
+    );
 };
 
 export default Cart;
--- src/components/ProductListing/ProductListing.tsx
+++ src/components/ProductListing/ProductListing.tsx
@@ -1,47 +1,66 @@
 import React from 'react';
 import products from '../../data/products';
 import { FaHeartCirclePlus } from "react-icons/fa6";
-import { FaHeartCircleCheck } from "react-icons/fa6";
+//import { FaHeartCircleCheck } from "react-icons/fa6";
 import { BsCartPlusFill } from "react-icons/bs";
 import { BsFillCartCheckFill } from "react-icons/bs";
+import { useCart } from '../Cart/CartContext';
 
 const ProductListing: React.FC = () => {
-  return (
-    <div className='container mx-auto mt-8 px-4'>
-        <h1 className='text-2xl font-bold mb-8 pl-2 pb-2 border-b-4 border-red-600'>
-            NOWE PRODUKTY
-        </h1>
-        <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2 cursor-pointer px-2'>
-            {products.map(product => (
-                <div key={product.id} className='relative hover:bg-gray-100 duration-300'>
-                    <img
-                        src={product.image}
-                        alt={product.title}
-                        className='w-full h-96 object-cover rounded-t-md'
-                    />
-                    <div className='border-b border-x p-2 rounded-b-md'>
-                        <div className='flex justify-between items-center'>
-                            <h2 className="text-lg font-semibold truncate">
-                                {product.title}
-                            </h2>
-                            <div className='flex gap-2 text-2xl cursor-pointer text-gray-900'>
-                                <FaHeartCirclePlus
-                                    className='hover:text-red-600 duration-300' 
-                                />
-                                <BsCartPlusFill
-                                    className='hover:text-green-600 duration-300'
-                                />
+    const { cart, addToCart, removeFromCart } = useCart();
+
+    const handleAddToCart = (product: { id: number; image: string; title: string; price: number; }) => {
+        if (cart.some(cartItem => cartItem.id === product.id)) {
+          removeFromCart(product.id);
+        } else {
+          addToCart(product);
+        }
+      };
+
+    return (
+        <div className='container mx-auto mt-8 px-4'>
+            <h1 className='text-2xl font-bold mb-8 pl-2 pb-2 border-b-4 border-red-600'>
+                NOWE PRODUKTY
+            </h1>
+            <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2 cursor-pointer px-2'>
+                {products.map(product => (
+                    <div key={product.id} className='relative hover:bg-gray-100 duration-300'>
+                        <img
+                            src={product.image}
+                            alt={product.title}
+                            className='w-full h-96 object-cover rounded-t-md'
+                        />
+                        <div className='border-b border-x p-2 rounded-b-md'>
+                            <div className='flex justify-between items-center'>
+                                <h2 className="text-lg font-semibold truncate">
+                                    {product.title}
+                                </h2>
+                                <div className='flex gap-2 text-2xl cursor-pointer text-gray-900'>
+                                    <FaHeartCirclePlus
+                                        className='hover:text-red-600 duration-300' 
+                                    />
+                                    {cart.some(cartItem => cartItem.id === product.id) ? (
+                                        <BsFillCartCheckFill
+                                            className='text-green-600'
+                                            onClick={() => handleAddToCart(product)}
+                                        />
+                                    ) : (
+                                        <BsCartPlusFill
+                                            className='hover:text-green-600 duration-300'
+                                            onClick={() => handleAddToCart(product)}
+                                        />
+                                    )}
+                                </div>
                             </div>
+                            <p className='text-red-600 font-semibold'>
+                                {product.price.toFixed(1)} PLN
+                            </p>
                         </div>
-                        <p className='text-red-600 font-semibold'>
-                            {product.price.toFixed(1)} PLN
-                        </p>
                     </div>
-                </div>
-            ))}
+                ))}
+            </div>
         </div>
-    </div>
-  );
+    );
 };
 
 export default ProductListing;
