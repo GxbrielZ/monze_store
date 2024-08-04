23cc9888d0ea2c212c6b2d7d918e69572ea5e7fb
Gabriel Zalewski <gabrielz2702@gmail.com>
2024-08-04 12:06:50 +0200

    Add cart and favourite icons. Change styling of boxes with products.

--- src/components/ProductListing/ProductListing.tsx
+++ src/components/ProductListing/ProductListing.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import products from '../../data/products';
+import { FaHeartCirclePlus } from "react-icons/fa6";
+import { FaHeartCircleCheck } from "react-icons/fa6";
+import { BsCartPlusFill } from "react-icons/bs";
+import { BsFillCartCheckFill } from "react-icons/bs";
 
 const ProductListing: React.FC = () => {
   return (
@@ -9,14 +13,30 @@ const ProductListing: React.FC = () => {
         </h1>
         <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2 cursor-pointer px-2'>
             {products.map(product => (
-                <div key={product.id} className='border p-4 relative hover:bg-gray-100 duration-300 rounded-md'>
-                    <img src={product.image} alt={product.title} className='w-full h-96 object-cover mb-2' />
-                    <h2 className="text-lg font-semibold truncate">
-                        {product.title}
-                    </h2>
-                    <p className="text-red-600 font-semibold">
-                        {product.price.toFixed(1)} PLN
-                    </p>
+                <div key={product.id} className='relative hover:bg-gray-100 duration-300'>
+                    <img
+                        src={product.image}
+                        alt={product.title}
+                        className='w-full h-96 object-cover rounded-t-md'
+                    />
+                    <div className='border-b border-x p-2 rounded-b-md'>
+                        <div className='flex justify-between items-center'>
+                            <h2 className="text-lg font-semibold truncate">
+                                {product.title}
+                            </h2>
+                            <div className='flex gap-2 text-2xl cursor-pointer text-gray-900'>
+                                <FaHeartCirclePlus
+                                    className='hover:text-red-600 duration-300' 
+                                />
+                                <BsCartPlusFill
+                                    className='hover:text-green-600 duration-300'
+                                />
+                            </div>
+                        </div>
+                        <p className='text-red-600 font-semibold'>
+                            {product.price.toFixed(1)} PLN
+                        </p>
+                    </div>
                 </div>
             ))}
         </div>
