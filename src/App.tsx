3a02ac747b7ac5234e3bf7df11c6fd1da991f9e0
Gabriel Zalewski <gabrielz2702@gmail.com>
2024-08-04 17:27:43 +0200

    Wrap App component with CartProvider to use cart functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar';
 import Home from './pages/Home';
+import { CartProvider } from './components/Cart/CartContext';
 
 const App: React.FC = () => {
   return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </Router>
+    <CartProvider>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
+    </CartProvider>
   );
 }
 
